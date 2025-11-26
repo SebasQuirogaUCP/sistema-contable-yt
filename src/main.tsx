a055fe2b3d6db293b5f1fdc2544d28ff59493af8
Sebastian Quiroga <sebastian.quiroga@synerio.com>
2025-11-26 14:11:59 -0500

    Corrected

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import App from "./App.tsx";
 import { Dashboard } from "./components/Dashboard.tsx";
 import { TransactionTable } from "./components/TransactionTable.tsx";
@@ -8,10 +8,10 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/sistema-contable-yt">
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route index element={<Navigate to="dashboard" replace />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="transacciones" element={<TransactionTable />} />
         </Route>
